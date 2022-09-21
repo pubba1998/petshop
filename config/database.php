2cd0f625c10b6c7dc32ab8a9f02d7f5768e4f099
pubba98 <coolpubba@gmail.com>
2022-09-21 15:12:29 +1200

    type mistake

--- config/database.php
+++ config/database.php
@@ -68,7 +68,7 @@ return [
             'url' => env('DATABASE_URL'),
             'host' => $DATABASE_URL['host'],
             'port' => $DATABASE_URL['port'],
-            'database' => eltrim($DATABASE_URL['path'], "/"),
+            'database' => ltrim($DATABASE_URL['path'], "/"),
             'username' => $DATABASE_URL['user'],
             'password' => $DATABASE_URL['pass'],
             'charset' => 'utf8',
